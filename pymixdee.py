c9478a36a9df8e3ea163a527165b481ced5af1e2
Pyonysos <87151917+Pyonysos@users.noreply.github.com>
2022-06-10 18:46:36 +0200

    improvements

--- pymixdee.py
+++ pymixdee.py
@@ -1,38 +1,80 @@
+"""
+The pymixdee package is designed to help the scientist, statistician, etc., to construct appropriate mixture designs.
+"""
+
 import numpy as np
+from typing import Union
 
 class MixD:
-    def __init__(self, nfact=None, factor_names=None):
+    """
+    MixD class:
+        attributes:
+            nfact:      int, number of factors
+            names:      str, list. names of the factors 
+            row_limit:  int, maximum number of experiments
+        methods:
+    """
+
+    def __init__(self, nfact : int, factor_names : list = None, row_limit : int = None):
         self.nfact = nfact
         self.names = factor_names
-    
+        self.row_limit = row_limit
+        self.with_mixd = False
+        self.with_constraints = False
+
     def __diag(self, n):
+        """
+        """
         return np.eye(n)
     
     def __dirichlet(self, size, alpha=None):
+        """
+        """
         if alpha == None:
             alpha = [1] * self.nfact
         return np.random.default_rng().dirichlet(alpha, size)
     
+    def add_constraints(self, constraints: Union[dict, list]):
+        self.with_constraints = True
+    
     def simplex_centroid(self, nfact, domain=None):
+        """
+        """
         ...
 
     def scheffe_network(self):
+        """
+        """
         ...
     
-    def mixD(self, type=None):
-        if type == 'centroid':
+    def generate(self, design = None):
+        """
+        """
+        if design in ('centroid', 'sc'):
             simplex_centroid()
 
-        if type == 'lattice':
+        elif design in ('lattice', 'scheffe', 'sn'):
             scheffe_network()
 
+        elif design in ('doptimal'):
+            pass
+
+        elif design in ('dirichlet'):
+            pass
+
     def d_optimal(self):
+        """
+        """
         ...
 
     def cmixd(self, nfact, ):
+        """
+        """
         ...
 
-
-
     def plot(self):
+        """
+        """
+        if self.with_mixd:
+            pass
         ...
